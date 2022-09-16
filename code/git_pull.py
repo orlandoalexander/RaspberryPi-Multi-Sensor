94d26a63799c776322d6f0988066d050b81bd53e
EcoSwell <info@ecoswell.org>
2022-09-16 10:27:29 -0500

    updates

--- code/git_pull.py
+++ code/git_pull.py
@@ -16,7 +16,7 @@ time.sleep(10) # delay to allow Raspberry Pi to connect to WiFi
 try: 
     requests.get('https://www.google.com/') # check if Raspberry Pi is connected to internet (request will cause error if not connected to internet --> except statement triggered)
     repo = Repo('') # access local git repo (current directory)
-    origin = repo.remotes.origin # access remote git repo 
+    origin = repo.remotes.origin # access remote git repo
     origin.pull() # pull latest changes from git repo to update sensor settings
     git_message = 'Successfully retrieved latest changes from GitHub Repo'
 except:
