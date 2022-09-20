7f6c745b70fc6a784dccf8a71a676e0316ad30b8
EcoSwell <info@ecoswell.org>
2022-09-20 17:42:40 -0500

    u

--- code/git_pull.py
+++ code/git_pull.py
@@ -11,7 +11,7 @@ from lcd_display import display_text, backlight_off, backlight_on
 
 
 #time.sleep(10) # delay to allow Raspberry Pi to connect to WiFi
-
+print('h')
 requests.get('https://www.google.com/') # check if Raspberry Pi is connected to internet (request will cause error if not connected to internet --> except statement triggered)
 repo = Repo('/home/ecoswell/RaspberryPi-Sensor/') # access local git repo
 origin = repo.remotes.origin # access remote git repo
