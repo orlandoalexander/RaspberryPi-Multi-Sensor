921243b1e05cdd4fc2b910a378f795c4c55e61d3
EcoSwell <info@ecoswell.org>
2022-09-20 12:12:32 -0500

    u

--- code/git_push.py
+++ code/git_push.py
@@ -7,6 +7,18 @@ from lcd_display import display_text, backlight_off, backlight_on
 
 #time.sleep(20) # delay to allow Raspberry Pi to connect to WiFi and latest changes to be pulled from git repo
 time.sleep(2)
+requests.get('https://www.google.com/') # check if Raspberry Pi is connected to internet (request will cause error if not connected to internet --> except statement triggered)
+repo = Repo('/home/ecoswell/RaspberryPi-Sensor/') # access local git repo
+repo.index.add('**') # stage all files for commit
+repo.index.commit('upload latest data files') # commit all staged files
+origin = repo.remotes.origin # access remote git repo 
+origin.push() # push all commits to remote git repo
+backlight_on() # turn on LCD backlight
+display_text('Latest version of data\n files will be uploaded\n to GitHub repo\n every 60s',13) # display git push status on LCD screen
+print('Git push - success')
+time.sleep(30)
+display_text('',1)
+backlight_off() # turn off LCD backlight
 while True:
     try: 
         requests.get('https://www.google.com/') # check if Raspberry Pi is connected to internet (request will cause error if not connected to internet --> except statement triggered)
