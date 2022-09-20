99ce58138a3724bb596e0ec27f2740c2e604548c
EcoSwell <info@ecoswell.org>
2022-09-20 18:07:52 -0500

    u

--- code/git_pull.py
+++ code/git_pull.py
@@ -10,21 +10,8 @@ import time
 from lcd_display import display_text, backlight_off, backlight_on
 
 
-time.sleep(10) # delay to allow Raspberry Pi to connect to WiFi
-
-requests.get('https://www.google.com/') # check if Raspberry Pi is connected to internet (request will cause error if not connected to internet --> except statement triggered)
-repo = Repo('/home/ecoswell/RaspberryPi-Sensor/') # access local git repo
-origin = repo.remotes.origin # access remote git repo
-origin.pull() # pull latest changes from git repo to update sensor settings
-backlight_on() # turn on LCD backlight
-display_text('Successfully retrieved\n sensor changes \nfrom Github repo',15) # display status message on LCD 
-time.sleep(5)
-display_text('Latest version of data\n files will be uploaded\n to GitHub repo\n every 60s',14) # display git push status on LCD screen
-time.sleep(5)
-print('Git pull - success')
 
 def test():
-
     try: 
         requests.get('https://www.google.com/') # check if Raspberry Pi is connected to internet (request will cause error if not connected to internet --> except statement triggered)
         repo = Repo('/home/ecoswell/RaspberryPi-Sensor/') # access local git repo
