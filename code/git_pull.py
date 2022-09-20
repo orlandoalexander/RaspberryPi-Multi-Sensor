2218d27652afc183fc1edfde881098d7829407e7
EcoSwell <info@ecoswell.org>
2022-09-20 18:17:41 -0500

    u

--- code/git_pull.py
+++ code/git_pull.py
@@ -11,24 +11,23 @@ from lcd_display import display_text, backlight_off, backlight_on
 
 
 
-def test():
-    time.sleep(10)
-    try: 
-        requests.get('https://www.google.com/') # check if Raspberry Pi is connected to internet (request will cause error if not connected to internet --> except statement triggered)
-        repo = Repo('/home/ecoswell/RaspberryPi-Sensor/') # access local git repo
-        origin = repo.remotes.origin # access remote git repo
-        origin.pull() # pull latest changes from git repo to update sensor settings
-        backlight_on() # turn on LCD backlight
-        display_text('Successfully retrieved\n sensor changes \nfrom Github repo',15) # display status message on LCD 
-        time.sleep(5)
-        display_text('Latest version of data\n files will be uploaded\n to GitHub repo\n every 60s',14) # display git push status on LCD screen
-        time.sleep(5)
-        print('Git pull - success')
-    except:
-        backlight_on() # turn on LCD backlight
-        display_text('Failed to retrieve\nsensor changes from \nGitHub repo.\nNo internet connection.',13) # display status message on LCD 
-        time.sleep(5)
-        display_text('Cannot upload latest\n version of data files \nto GitHub Repo.\nNo internet connection.',13) # display git push status on LCD screen
-        time.sleep(5)
-        print('Git pull - fail')
+time.sleep(10)
+try: 
+    requests.get('https://www.google.com/') # check if Raspberry Pi is connected to internet (request will cause error if not connected to internet --> except statement triggered)
+    repo = Repo('/home/ecoswell/RaspberryPi-Sensor/') # access local git repo
+    origin = repo.remotes.origin # access remote git repo
+    origin.pull() # pull latest changes from git repo to update sensor settings
+    backlight_on() # turn on LCD backlight
+    display_text('Successfully retrieved\n sensor changes \nfrom Github repo',15) # display status message on LCD 
+    time.sleep(5)
+    display_text('Latest version of data\n files will be uploaded\n to GitHub repo\n every 60s',14) # display git push status on LCD screen
+    time.sleep(5)
+    print('Git pull - success')
+except:
+    backlight_on() # turn on LCD backlight
+    display_text('Failed to retrieve\nsensor changes from \nGitHub repo.\nNo internet connection.',13) # display status message on LCD 
+    time.sleep(5)
+    display_text('Cannot upload latest\n version of data files \nto GitHub Repo.\nNo internet connection.',13) # display git push status on LCD screen
+    time.sleep(5)
+    print('Git pull - fail')
 
