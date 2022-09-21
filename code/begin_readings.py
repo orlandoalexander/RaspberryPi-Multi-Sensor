9e36635a92c05615c958caa390d94247dee89418
EcoSwell <113472300+EcoSwell@users.noreply.github.com>
2022-09-21 00:50:12 -0500

    Update begin_readings.py

--- code/begin_readings.py
+++ code/begin_readings.py
@@ -11,7 +11,7 @@ import time
 import threading
 from sensor_readings import SensorReadings
 from lcd_display import display_text, backlight_off, backlight_on
-print("heyyy what's going")
+print("heyyy what's going on world")
 
 try:
     # transitional fix for breaking change in LTR559
