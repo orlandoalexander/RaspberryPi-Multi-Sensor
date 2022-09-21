1fb967abcf2617635acca22e1da997d5e3caa1f5
EcoSwell <info@ecoswell.org>
2022-09-21 01:22:05 -0500

    u

--- code/git_push.py
+++ code/git_push.py
@@ -9,20 +9,25 @@ from git import Repo
 from lcd_display import display_text, backlight_off, backlight_on
 
 time.sleep(2) # delay to allow Raspberry Pi to connect to WiFi and latest changes to be pulled from git repo
+repo = Repo('/home/ecoswell/RaspberryPi-Sensor') # access local git repo
 
-while True:
-    try: 
-        time.sleep(2)
-        requests.get('https://www.google.com/') # check if Raspberry Pi is connected to internet (request will cause error if not connected to internet --> except statement triggered)
-        repo = Repo('/home/ecoswell/RaspberryPi-Sensor/') # access local git repo
-        repo.index.add('**') # stage all files for commit
-        repo.index.commit('upload latest data files') # commit all staged files
-        origin = repo.remotes.origin # access remote git repo 
-        origin.pull () # pull before push to ensure successfull pull
-        origin.push() # push all commits to remote git repo
-        print('Git push - success')
-        sleep_time = 60 # following successful push sleep for 60 secs
-    except:
-        print('Git push - fail')
-        sleep_time = 0 # following unsuccessful push sleep for 0 secs
-    time.sleep(sleep_time)
+if len(repo.untracked_files):    
+    repo.git.add(A=True)
+    repo.git.commit('-m', 'initial commit')
+    repo.git.push('origin', 'HEAD:refs/for/master')
+# while True:
+#     try: 
+#         time.sleep(2)
+#         requests.get('https://www.google.com/') # check if Raspberry Pi is connected to internet (request will cause error if not connected to internet --> except statement triggered)
+#         repo = Repo('/home/ecoswell/RaspberryPi-Sensor/') # access local git repo
+#         repo.index.add('**') # stage all files for commit
+#         repo.index.commit('upload latest data files') # commit all staged files
+#         origin = repo.remotes.origin # access remote git repo 
+#         origin.pull () # pull before push to ensure successfull pull
+#         origin.push() # push all commits to remote git repo
+#         print('Git push - success')
+#         sleep_time = 60 # following successful push sleep for 60 secs
+#     except:
+#         print('Git push - fail')
+#         sleep_time = 0 # following unsuccessful push sleep for 0 secs
+#     time.sleep(sleep_time)
