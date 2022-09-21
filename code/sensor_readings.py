c497f86b786df028381720af35da372d20005f97
EcoSwell <info@ecoswell.org>
2022-09-21 03:55:24 -0500

    u

--- code/sensor_readings.py
+++ code/sensor_readings.py
@@ -281,7 +281,6 @@ class SensorReadings(): # class containing methods to take sensor readings
     def save_data(self, sensor, freq, dur, data, data_heading): # save sensor data to CSV file
         data = [round(i, 2) for i in data] # round data values to 2 dp - must iterate over each element in list as data values stored in list
         filename = sensor+'-'+self.date+'-'+self.time+'.csv' # filename stores sensor type and current date
-        backlight_on()
         if os.path.isfile(f'/home/ecoswell/RaspberryPi-Sensor/data/{filename}'): # if CSV file storing data for 'sensor' already exists
             f = open(f'/home/ecoswell/RaspberryPi-Sensor/data/{filename}', 'a') # create/open CSV file to store data for 'sensor'
             writer = csv.writer(f)
