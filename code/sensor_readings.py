d9db75f75dbf7c0320374ab18d35f999cc60e0df
EcoSwell <info@ecoswell.org>
2022-09-29 17:07:57 -0500

    updates

--- code/sensor_readings.py
+++ code/sensor_readings.py
@@ -92,12 +92,12 @@ class SensorReadings(): # class containing methods to take sensor readings
             gas_readings[0] += co # add together all co readings
             gas_readings[1] += no2 # add together all no2 readings
             gas_readings[2] += nh3 # add together all nh3 readings
-            time.sleep(60)
+            time.sleep(5)
         co_R0 = str(round(gas_readings[0]/10,2)) # store average R0 value for co 
         no2_R0 = str(round(gas_readings[1]/10,2)) # store average R0 value for no2 
         nh3_R0 = str(round(gas_readings[2]/10,2)) # store average R0 value for nh3 
         print(co_R0)
-        with open('code/gas_factors.txt','w') as f:
+        with open('gas_factors.txt','w') as f:
             f.write(f'{co_R0}\n{no2_R0}\n{nh3_R0}') # write R0 value of each gas to text file
             f.close()
         print('file saved')
