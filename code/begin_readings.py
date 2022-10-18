e15dd31cb073fd450bf7a7dcbd7310b6a4dca51a
EcoSwell <113472300+EcoSwell@users.noreply.github.com>
2022-10-18 11:26:55 -0500

    Update begin_readings.py
    Signed-off-by: EcoSwell <113472300+EcoSwell@users.noreply.github.com>

--- code/begin_readings.py
+++ code/begin_readings.py
@@ -17,9 +17,7 @@ try:
     # transitional fix for breaking change in LTR559
     from ltr559 import LTR559
     ltr559 = LTR559() # initialise LTR559 light/proximity sensor
-    display_text('Light initialised', 20)
 except ImportError:
-    display_text('Light not initialised', 20)
     import ltr559
    
 
