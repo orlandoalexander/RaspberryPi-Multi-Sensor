19da390b35adb11d00b88df2caf6a14dfb4d600f
EcoSwell <113472300+EcoSwell@users.noreply.github.com>
2022-10-19 15:08:27 -0500

    Update sensor_settings.py
    Signed-off-by: EcoSwell <113472300+EcoSwell@users.noreply.github.com>

--- sensor_settings.py
+++ sensor_settings.py
@@ -27,7 +27,7 @@ Modify the values inside the brackets below for each active sensor with the foll
 i.e. to measure temperature and ammonia with a time delay between readings of 60 and 120 secs 
 and a data recording duration of 4 hours and 7 hours respectively, you should write [(1,60,240), (7,120,420)]
 '''
-sensors = [(1,60,240), (7,120,420)]
+sensors = [(1,60,60), (2,60,60), (3,60,60), (4,60,60), (5,60,60), (6,60,60), (7,60,60), (8,60,60)]
 
 
 
@@ -39,7 +39,7 @@ To determine the factor required to correctly compensate the tenmperature readin
 set 'calculate_temp_factor' to True and follow the instructions in the documentation (README.md).
 Note: if you want to calculate the temperature tuning factor, 'calculate_gas_factor' must be set to False.
 '''
-calculate_temp_factor = True
+calculate_temp_factor = False
 
 factor = 1.31
 
@@ -51,7 +51,7 @@ To obtain gas readings in ppm, you must first calibrate the sensors by taking re
 To calibrate the gas sensors, set 'calculate_gas_factor' to True and follow the instructions in the documentation (README.md).
 Note: if you want to calculate the gas calibration factor, 'calculate_temp_factor' must be set to False.
 '''
-calculate_gas_factor = True
+calculate_gas_factor = False
 
 """
 TECHNICAL INFORMATION
